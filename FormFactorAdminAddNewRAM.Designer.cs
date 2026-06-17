8a259d4d34b7e8966ee6236c025e4ff62d592759
lestersollano <lestermatthewsollano@gmail.com>
2026-06-17 12:04:23 +0800

    design ram

--- FormFactorAdminAddNewRAM.Designer.cs
+++ FormFactorAdminAddNewRAM.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormFactorAdminAddNewRAM));
             groupBox1 = new GroupBox();
             txtGhz = new TextBox();
             groupBox2 = new GroupBox();
@@ -50,18 +51,31 @@
             groupBox5 = new GroupBox();
             btnSave = new Button();
             btnCancel = new Button();
+            groupBox6 = new GroupBox();
+            textBox1 = new TextBox();
+            panel1 = new Panel();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            panel2 = new Panel();
+            panel3 = new Panel();
             groupBox1.SuspendLayout();
             groupBox2.SuspendLayout();
             groupBox3.SuspendLayout();
             groupBox4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)numQuantity).BeginInit();
             groupBox5.SuspendLayout();
+            groupBox6.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
             SuspendLayout();
             // 
             // groupBox1
             // 
             groupBox1.Controls.Add(txtGhz);
-            groupBox1.Location = new Point(12, 12);
+            groupBox1.Font = new Font("Gill Sans MT", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox1.Location = new Point(18, 14);
             groupBox1.Name = "groupBox1";
             groupBox1.Size = new Size(111, 60);
             groupBox1.TabIndex = 0;
@@ -70,7 +84,7 @@
             // 
             // txtGhz
             // 
-            txtGhz.Location = new Point(6, 24);
+            txtGhz.Location = new Point(6, 22);
             txtGhz.Name = "txtGhz";
             txtGhz.Size = new Size(99, 23);
             txtGhz.TabIndex = 0;
@@ -81,9 +95,10 @@
             groupBox2.Controls.Add(rbDDR3);
             groupBox2.Controls.Add(rbDDR4);
             groupBox2.Controls.Add(rbDDR5);
-            groupBox2.Location = new Point(12, 78);
+            groupBox2.Font = new Font("Gill Sans MT", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox2.Location = new Point(157, 15);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(111, 129);
+            groupBox2.Size = new Size(313, 60);
             groupBox2.TabIndex = 1;
             groupBox2.TabStop = false;
             groupBox2.Text = "Speed (Version)";
@@ -91,9 +106,9 @@
             // rbDDR2
             // 
             rbDDR2.AutoSize = true;
-            rbDDR2.Location = new Point(25, 98);
+            rbDDR2.Location = new Point(25, 23);
             rbDDR2.Name = "rbDDR2";
-            rbDDR2.Size = new Size(54, 19);
+            rbDDR2.Size = new Size(62, 22);
             rbDDR2.TabIndex = 3;
             rbDDR2.TabStop = true;
             rbDDR2.Text = "DDR2";
@@ -102,9 +117,9 @@
             // rbDDR3
             // 
             rbDDR3.AutoSize = true;
-            rbDDR3.Location = new Point(25, 73);
+            rbDDR3.Location = new Point(95, 23);
             rbDDR3.Name = "rbDDR3";
-            rbDDR3.Size = new Size(54, 19);
+            rbDDR3.Size = new Size(62, 22);
             rbDDR3.TabIndex = 2;
             rbDDR3.TabStop = true;
             rbDDR3.Text = "DDR3";
@@ -113,9 +128,9 @@
             // rbDDR4
             // 
             rbDDR4.AutoSize = true;
-            rbDDR4.Location = new Point(25, 48);
+            rbDDR4.Location = new Point(165, 23);
             rbDDR4.Name = "rbDDR4";
-            rbDDR4.Size = new Size(54, 19);
+            rbDDR4.Size = new Size(62, 22);
             rbDDR4.TabIndex = 1;
             rbDDR4.TabStop = true;
             rbDDR4.Text = "DDR4";
@@ -124,9 +139,9 @@
             // rbDDR5
             // 
             rbDDR5.AutoSize = true;
-            rbDDR5.Location = new Point(25, 23);
+            rbDDR5.Location = new Point(235, 23);
             rbDDR5.Name = "rbDDR5";
-            rbDDR5.Size = new Size(54, 19);
+            rbDDR5.Size = new Size(62, 22);
             rbDDR5.TabIndex = 0;
             rbDDR5.TabStop = true;
             rbDDR5.Text = "DDR5";
@@ -142,9 +157,10 @@
             groupBox3.Controls.Add(rb4);
             groupBox3.Controls.Add(rb2);
             groupBox3.Controls.Add(rb1);
-            groupBox3.Location = new Point(129, 12);
+            groupBox3.Font = new Font("Gill Sans MT", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox3.Location = new Point(486, 19);
             groupBox3.Name = "groupBox3";
-            groupBox3.Size = new Size(99, 133);
+            groupBox3.Size = new Size(344, 56);
             groupBox3.TabIndex = 1;
             groupBox3.TabStop = false;
             groupBox3.Text = "Capacity (GB)";
@@ -152,9 +168,9 @@
             // rb64
             // 
             rb64.AutoSize = true;
-            rb64.Location = new Point(43, 72);
+            rb64.Location = new Point(245, 22);
             rb64.Name = "rb64";
-            rb64.Size = new Size(37, 19);
+            rb64.Size = new Size(40, 22);
             rb64.TabIndex = 7;
             rb64.TabStop = true;
             rb64.Text = "64";
@@ -163,9 +179,9 @@
             // rb128
             // 
             rb128.AutoSize = true;
-            rb128.Location = new Point(43, 97);
+            rb128.Location = new Point(288, 22);
             rb128.Name = "rb128";
-            rb128.Size = new Size(43, 19);
+            rb128.Size = new Size(47, 22);
             rb128.TabIndex = 6;
             rb128.TabStop = true;
             rb128.Text = "128";
@@ -174,9 +190,9 @@
             // rb32
             // 
             rb32.AutoSize = true;
-            rb32.Location = new Point(43, 47);
+            rb32.Location = new Point(202, 22);
             rb32.Name = "rb32";
-            rb32.Size = new Size(37, 19);
+            rb32.Size = new Size(40, 22);
             rb32.TabIndex = 5;
             rb32.TabStop = true;
             rb32.Text = "32";
@@ -185,9 +201,9 @@
             // rb16
             // 
             rb16.AutoSize = true;
-            rb16.Location = new Point(43, 22);
+            rb16.Location = new Point(159, 22);
             rb16.Name = "rb16";
-            rb16.Size = new Size(37, 19);
+            rb16.Size = new Size(40, 22);
             rb16.TabIndex = 4;
             rb16.TabStop = true;
             rb16.Text = "16";
@@ -196,9 +212,9 @@
             // rb8
             // 
             rb8.AutoSize = true;
-            rb8.Location = new Point(6, 97);
+            rb8.Location = new Point(122, 22);
             rb8.Name = "rb8";
-            rb8.Size = new Size(31, 19);
+            rb8.Size = new Size(33, 22);
             rb8.TabIndex = 3;
             rb8.TabStop = true;
             rb8.Text = "8";
@@ -207,9 +223,9 @@
             // rb4
             // 
             rb4.AutoSize = true;
-            rb4.Location = new Point(6, 72);
+            rb4.Location = new Point(85, 22);
             rb4.Name = "rb4";
-            rb4.Size = new Size(31, 19);
+            rb4.Size = new Size(33, 22);
             rb4.TabIndex = 2;
             rb4.TabStop = true;
             rb4.Text = "4";
@@ -218,9 +234,9 @@
             // rb2
             // 
             rb2.AutoSize = true;
-            rb2.Location = new Point(6, 47);
+            rb2.Location = new Point(48, 22);
             rb2.Name = "rb2";
-            rb2.Size = new Size(31, 19);
+            rb2.Size = new Size(33, 22);
             rb2.TabIndex = 1;
             rb2.TabStop = true;
             rb2.Text = "2";
@@ -229,9 +245,9 @@
             // rb1
             // 
             rb1.AutoSize = true;
-            rb1.Location = new Point(6, 22);
+            rb1.Location = new Point(11, 22);
             rb1.Name = "rb1";
-            rb1.Size = new Size(31, 19);
+            rb1.Size = new Size(33, 22);
             rb1.TabIndex = 0;
             rb1.TabStop = true;
             rb1.Text = "1";
@@ -240,7 +256,7 @@
             // groupBox4
             // 
             groupBox4.Controls.Add(txtName);
-            groupBox4.Location = new Point(12, 213);
+            groupBox4.Location = new Point(13, 511);
             groupBox4.Name = "groupBox4";
             groupBox4.Size = new Size(216, 56);
             groupBox4.TabIndex = 2;
@@ -264,7 +280,8 @@
             // groupBox5
             // 
             groupBox5.Controls.Add(numQuantity);
-            groupBox5.Location = new Point(12, 275);
+            groupBox5.Font = new Font("Gill Sans MT", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox5.Location = new Point(31, 329);
             groupBox5.Name = "groupBox5";
             groupBox5.Size = new Size(216, 56);
             groupBox5.TabIndex = 4;
@@ -273,7 +290,7 @@
             // 
             // btnSave
             // 
-            btnSave.Location = new Point(153, 337);
+            btnSave.Location = new Point(148, 463);
             btnSave.Name = "btnSave";
             btnSave.Size = new Size(75, 23);
             btnSave.TabIndex = 5;
@@ -283,7 +300,7 @@
             // 
             // btnCancel
             // 
-            btnCancel.Location = new Point(72, 337);
+            btnCancel.Location = new Point(67, 463);
             btnCancel.Name = "btnCancel";
             btnCancel.Size = new Size(75, 23);
             btnCancel.TabIndex = 6;
@@ -291,18 +308,88 @@
             btnCancel.UseVisualStyleBackColor = true;
             btnCancel.Click += btnCancel_Click;
             // 
+            // groupBox6
+            // 
+            groupBox6.Controls.Add(textBox1);
+            groupBox6.Location = new Point(300, 452);
+            groupBox6.Name = "groupBox6";
+            groupBox6.Size = new Size(135, 48);
+            groupBox6.TabIndex = 7;
+            groupBox6.TabStop = false;
+            groupBox6.Text = "Price";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(6, 19);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(123, 23);
+            textBox1.TabIndex = 8;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Black;
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(850, 36);
+            panel1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Gill Sans MT", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(6, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(105, 18);
+            label1.TabIndex = 9;
+            label1.Text = "Memory (RAM)";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 54);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(850, 153);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 9;
+            pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(224, 224, 224);
+            panel2.BorderStyle = BorderStyle.FixedSingle;
+            panel2.Controls.Add(panel3);
+            panel2.Controls.Add(groupBox2);
+            panel2.Controls.Add(groupBox3);
+            panel2.Location = new Point(12, 213);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(850, 99);
+            panel2.TabIndex = 10;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.Silver;
+            panel3.Controls.Add(groupBox1);
+            panel3.Location = new Point(6, 5);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(154, 89);
+            panel3.TabIndex = 2;
+            // 
             // FormFactorAdminAddNewRAM
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(241, 370);
+            BackColor = Color.White;
+            ClientSize = new Size(874, 635);
+            Controls.Add(panel2);
+            Controls.Add(pictureBox1);
+            Controls.Add(panel1);
+            Controls.Add(groupBox6);
             Controls.Add(btnCancel);
             Controls.Add(btnSave);
             Controls.Add(groupBox5);
             Controls.Add(groupBox4);
-            Controls.Add(groupBox3);
-            Controls.Add(groupBox2);
-            Controls.Add(groupBox1);
             Name = "FormFactorAdminAddNewRAM";
             Text = "FormFactorAdminAddNewRAM";
             groupBox1.ResumeLayout(false);
@@ -315,6 +402,13 @@
             groupBox4.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)numQuantity).EndInit();
             groupBox5.ResumeLayout(false);
+            groupBox6.ResumeLayout(false);
+            groupBox6.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel3.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -342,5 +436,12 @@
         private GroupBox groupBox5;
         private Button btnSave;
         private Button btnCancel;
+        private GroupBox groupBox6;
+        private TextBox textBox1;
+        private Panel panel1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Panel panel2;
+        private Panel panel3;
     }
 }
