f7c5097d68ec4277c67f5987cd9222eac468b1bd
Matthew <lestermatthewsollano@gmail.com>
2026-06-17 15:12:50 +0800

    fix textbox error

--- UserSelect.Designer.cs
+++ UserSelect.Designer.cs
@@ -33,13 +33,8 @@
             textBox1 = new TextBox();
             button2 = new Button();
             flowLayoutPanel1 = new FlowLayoutPanel();
-            panel1 = new Panel();
-            label6 = new Label();
-            panel2 = new Panel();
-            panel3 = new Panel();
-            label8 = new Label();
             panel4 = new Panel();
-            button1 = new Button();
+            btnLoginAdmin = new Button();
             panel5 = new Panel();
             label7 = new Label();
             label9 = new Label();
@@ -47,17 +42,24 @@
             label10 = new Label();
             textBox3 = new TextBox();
             label11 = new Label();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            label8 = new Label();
+            panel1 = new Panel();
+            label6 = new Label();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
             flowLayoutPanel1.SuspendLayout();
-            panel1.SuspendLayout();
-            panel2.SuspendLayout();
-            panel3.SuspendLayout();
             panel4.SuspendLayout();
             panel5.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
-            label1.Font = new Font("Gill Sans MT", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label1.Location = new Point(3, 40);
             label1.Name = "label1";
             label1.Size = new Size(291, 52);
@@ -67,27 +69,27 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Font = new Font("Gill Sans MT", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label5.Location = new Point(3, 95);
             label5.Name = "label5";
-            label5.Size = new Size(98, 18);
+            label5.Size = new Size(108, 16);
             label5.TabIndex = 8;
             label5.Text = "Email Address";
             // 
             // textBox1
             // 
             textBox1.BorderStyle = BorderStyle.FixedSingle;
-            textBox1.Location = new Point(3, 118);
+            textBox1.Location = new Point(6, 89);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(291, 23);
+            textBox1.Size = new Size(100, 23);
             textBox1.TabIndex = 7;
             // 
             // button2
             // 
-            button2.Font = new Font("Gill Sans MT", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button2.Location = new Point(198, 203);
+            button2.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(187, 162);
             button2.Name = "button2";
-            button2.Size = new Size(96, 31);
+            button2.Size = new Size(75, 23);
             button2.TabIndex = 6;
             button2.Text = "Start";
             button2.UseVisualStyleBackColor = true;
@@ -103,26 +105,97 @@
             flowLayoutPanel1.Size = new Size(614, 254);
             flowLayoutPanel1.TabIndex = 7;
             // 
-            // panel1
+            // panel4
             // 
-            panel1.BackColor = Color.FromArgb(98, 178, 54);
-            panel1.BorderStyle = BorderStyle.FixedSingle;
-            panel1.Controls.Add(label6);
-            panel1.Location = new Point(0, -1);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(614, 60);
-            panel1.TabIndex = 8;
+            panel4.BackColor = Color.FromArgb(224, 224, 224);
+            panel4.BorderStyle = BorderStyle.FixedSingle;
+            panel4.Controls.Add(txtPassword);
+            panel4.Controls.Add(txtUsername);
+            panel4.Controls.Add(btnLoginAdmin);
+            panel4.Controls.Add(panel5);
+            panel4.Controls.Add(label9);
+            panel4.Controls.Add(textBox2);
+            panel4.Controls.Add(label10);
+            panel4.Controls.Add(textBox3);
+            panel4.Controls.Add(label11);
+            panel4.Location = new Point(3, 3);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(300, 242);
+            panel4.TabIndex = 6;
             // 
-            // label6
+            // btnLoginAdmin
             // 
-            label6.AutoSize = true;
-            label6.Font = new Font("Bauhaus 93", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label6.ForeColor = Color.White;
-            label6.Location = new Point(183, 0);
-            label6.Name = "label6";
-            label6.Size = new Size(271, 54);
-            label6.TabIndex = 0;
-            label6.Text = "FormFactor";
+            btnLoginAdmin.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnLoginAdmin.Location = new Point(198, 203);
+            btnLoginAdmin.Name = "btnLoginAdmin";
+            btnLoginAdmin.Size = new Size(96, 31);
+            btnLoginAdmin.TabIndex = 5;
+            btnLoginAdmin.Text = "Login";
+            btnLoginAdmin.UseVisualStyleBackColor = true;
+            btnLoginAdmin.Click += btnLoginAdmin_Click;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.Black;
+            panel5.Controls.Add(label7);
+            panel5.Location = new Point(-1, -1);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(300, 38);
+            panel5.TabIndex = 1;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.White;
+            label7.Location = new Point(95, 9);
+            label7.Name = "label7";
+            label7.Size = new Size(109, 18);
+            label7.TabIndex = 2;
+            label7.Text = "Admin Contol";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.Location = new Point(3, 142);
+            label9.Name = "label9";
+            label9.Size = new Size(75, 16);
+            label9.TabIndex = 3;
+            label9.Text = "Password";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(0, 0);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.Location = new Point(3, 95);
+            label10.Name = "label10";
+            label10.Size = new Size(78, 16);
+            label10.TabIndex = 4;
+            label10.Text = "Username";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(0, 0);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 7;
+            // 
+            // label11
+            // 
+            label11.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label11.Location = new Point(3, 40);
+            label11.Name = "label11";
+            label11.Size = new Size(291, 52);
+            label11.TabIndex = 0;
+            label11.Text = "Admins can enter their credentials here to manage the system.";
             // 
             // panel2
             // 
@@ -150,106 +223,50 @@
             // label8
             // 
             label8.AutoSize = true;
-            label8.Font = new Font("Gill Sans MT", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label8.ForeColor = Color.White;
             label8.Location = new Point(95, 9);
             label8.Name = "label8";
-            label8.Size = new Size(113, 21);
+            label8.Size = new Size(119, 18);
             label8.TabIndex = 2;
             label8.Text = "User Shopping";
             // 
-            // panel4
+            // panel1
             // 
-            panel4.BackColor = Color.FromArgb(224, 224, 224);
-            panel4.BorderStyle = BorderStyle.FixedSingle;
-            panel4.Controls.Add(button1);
-            panel4.Controls.Add(panel5);
-            panel4.Controls.Add(label9);
-            panel4.Controls.Add(textBox2);
-            panel4.Controls.Add(label10);
-            panel4.Controls.Add(textBox3);
-            panel4.Controls.Add(label11);
-            panel4.Location = new Point(3, 3);
-            panel4.Name = "panel4";
-            panel4.Size = new Size(300, 242);
-            panel4.TabIndex = 6;
+            panel1.BackColor = Color.FromArgb(98, 178, 54);
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(label6);
+            panel1.Location = new Point(0, -1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(614, 60);
+            panel1.TabIndex = 8;
             // 
-            // button1
+            // label6
             // 
-            button1.Font = new Font("Gill Sans MT", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.Location = new Point(198, 203);
-            button1.Name = "button1";
-            button1.Size = new Size(96, 31);
-            button1.TabIndex = 5;
-            button1.Text = "Login";
-            button1.UseVisualStyleBackColor = true;
+            label6.AutoSize = true;
+            label6.Font = new Font("Microsoft Sans Serif", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(183, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(282, 55);
+            label6.TabIndex = 0;
+            label6.Text = "FormFactor";
             // 
-            // panel5
+            // txtUsername
             // 
-            panel5.BackColor = Color.Black;
-            panel5.Controls.Add(label7);
-            panel5.Location = new Point(-1, -1);
-            panel5.Name = "panel5";
-            panel5.Size = new Size(300, 38);
-            panel5.TabIndex = 1;
+            txtUsername.Location = new Point(3, 116);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(100, 23);
+            txtUsername.TabIndex = 8;
             // 
-            // label7
+            // txtPassword
             // 
-            label7.AutoSize = true;
-            label7.Font = new Font("Gill Sans MT", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label7.ForeColor = Color.White;
-            label7.Location = new Point(95, 9);
-            label7.Name = "label7";
-            label7.Size = new Size(110, 21);
-            label7.TabIndex = 2;
-            label7.Text = "Admin Contol";
+            txtPassword.Location = new Point(3, 161);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(100, 23);
+            txtPassword.TabIndex = 9;
             // 
-            // label9
-            // 
-            label9.AutoSize = true;
-            label9.Font = new Font("Gill Sans MT", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label9.Location = new Point(3, 142);
-            label9.Name = "label9";
-            label9.Size = new Size(68, 18);
-            label9.TabIndex = 3;
-            label9.Text = "Password";
-            // 
-            // textBox2
-            // 
-            button2.Location = new Point(187, 162);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 6;
-            button2.Text = "Start";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // label10
-            // 
-            label10.AutoSize = true;
-            label10.Font = new Font("Gill Sans MT", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label10.Location = new Point(3, 95);
-            label10.Name = "label10";
-            label10.Size = new Size(72, 18);
-            label10.TabIndex = 4;
-            label10.Text = "Username";
-            // 
-            // textBox3
-            // 
-            textBox1.Location = new Point(6, 89);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
-            textBox1.TabIndex = 7;
-            // 
-            // label11
-            // 
-            label11.Font = new Font("Gill Sans MT", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label11.Location = new Point(3, 40);
-            label11.Name = "label11";
-            label11.Size = new Size(291, 52);
-            label11.TabIndex = 0;
-            label11.Text = "Admins can enter their credentials here to manage the system.";
-            // 
-            // FormFactorUserSelect
+            // UserSelect
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
@@ -257,19 +274,19 @@
             ClientSize = new Size(619, 315);
             Controls.Add(flowLayoutPanel1);
             Controls.Add(panel1);
-            Name = "FormFactorUserSelect";
+            Name = "UserSelect";
             Text = "FormFactor - Select User";
             flowLayoutPanel1.ResumeLayout(false);
-            panel1.ResumeLayout(false);
-            panel1.PerformLayout();
-            panel2.ResumeLayout(false);
-            panel2.PerformLayout();
-            panel3.ResumeLayout(false);
-            panel3.PerformLayout();
             panel4.ResumeLayout(false);
             panel4.PerformLayout();
             panel5.ResumeLayout(false);
             panel5.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -286,7 +303,7 @@
         private Panel panel3;
         private Label label8;
         private Panel panel4;
-        private Button button1;
+        private Button btnLoginAdmin;
         private Panel panel5;
         private Label label7;
         private Label label9;
@@ -294,5 +311,7 @@
         private Label label10;
         private TextBox textBox3;
         private Label label11;
+        private TextBox txtPassword;
+        private TextBox txtUsername;
     }
 }
